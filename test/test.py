641cccd76dfdbf12ffafe0385fd47d15dff2a1d1
Gabriele Orlandi <gabriele.orlandi@xtreamers.io>
2021-03-02 14:14:52 +0100

    test: add working test (one func failing)

--- test/test.py
+++ test/test.py
@@ -1,36 +1,19 @@
 import os
 from copy import deepcopy
-from typing import Callable, Tuple
+from typing import ItemsView
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import pytest
 
 from pytsviz.global_vars import root_path
-from pytsviz.viz import plot_acf, plotly_pacf, plot_psd, plot_ts_analysis, plot_distribution, plot_gof, \
+from pytsviz.viz import plot_acf, plot_psd, plot_ts_analysis, plot_distribution, plot_gof, \
     plot_ts, plot_seasonal_ts, plot_decomposed_ts, plot_forecast, plot_scatter_matrix, \
     plot_scatter_fit, plot_inverse_arma_roots, plot_extended_scatter_matrix, plot_ts_overview
 
 data_path = os.path.join(root_path, "data", "crypto.csv")
 
 df = pd.read_csv(data_path, index_col=0, parse_dates=True, dayfirst=True)
-
-
-def test_args_are_unchanged(
-        func: Callable,
-        args: Tuple,
-        kwargs: dict
-):
-    input_args = deepcopy(args)
-    input_kwargs = deepcopy(kwargs)
-
-    func(args, kwargs)
-
-    for i in range(len(args)):
-        assert args[i] == input_args[i]
-
-    for i in range(len(kwargs)):
-        assert kwargs[i] == input_kwargs[i]
-
 
 testing_dict = {
     plot_acf: {
@@ -56,7 +39,7 @@ testing_dict = {
     },
     plot_ts: {
             "df": df,
-            "tf": "moving_average"
+            "tf": "Box-Cox"
     },
     plot_seasonal_ts: {
             "df": df,
@@ -65,7 +48,9 @@ testing_dict = {
     },
     plot_decomposed_ts: {
             "df": df,
+            "y_col": "LTC",
             "method": "STL",
+            "period": 3
     },
     plot_forecast: {
             "df": df,
@@ -93,6 +78,27 @@ testing_dict = {
             "df": df
     },
     plot_ts_overview: {
-            "series": df["LTC"]
+            "df": df,
+            "y_col": "LTC"
     }
 }
+
+func_names = [f.__name__ for f in testing_dict.keys()]
+
+
+@pytest.mark.parametrize("func_dict", testing_dict.items(), ids=func_names)
+def test_args_are_unchanged(
+        func_dict: ItemsView[str, dict],
+):
+    func, kwargs = func_dict
+    processed_kwargs = deepcopy(kwargs)
+
+    func(**processed_kwargs, show=False)
+
+    for k in kwargs.keys():
+        assert isinstance(kwargs[k], type(processed_kwargs[k]))
+        if isinstance(kwargs[k], pd.DataFrame):
+            pd.testing.assert_frame_equal(kwargs[k], processed_kwargs[k])
+        else:
+            assert kwargs[k] == processed_kwargs[k]
+
