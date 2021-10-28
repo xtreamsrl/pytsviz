e688165ee5a39ec1968ddb3b3dbf2e93b28ad38f
Riccardo Zoncada <riccardo.zoncada@xtreamers.io>
2021-10-28 13:06:03 +0200

    doc: remove napoleon sphinx extension, add autodoc type hints sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,20 +33,12 @@ extensions = [
     "myst_parser",
     "nbsphinx",
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
     "sphinx.ext.intersphinx",
 ]
 
-# Napoleon settings
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = True
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
+# nbsphinx settings
+nbsphinx_execute = 'always'
 
 # Intersphinx settings
 intersphinx_mapping = {
